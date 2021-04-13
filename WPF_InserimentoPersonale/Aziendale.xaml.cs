6dde287f8537c8b4f64ab926495f70f1878b8f1b
AlexBuratta13 <alex,buratta13@gmail.com>
2021-04-13 12:17:54 +0200

    Wpf aggoirnata

--- WPF_InserimentoPersonale/Aziendale.xaml.cs
+++ WPF_InserimentoPersonale/Aziendale.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ShowFile
+namespace WPF_InserimentoPersonale
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Aziendale.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Aziendale : Page
     {
-        public MainWindow()
+        public Aziendale()
         {
             InitializeComponent();
         }
--- WPF_InserimentoPersonale/MostraFile.xaml.cs
+++ WPF_InserimentoPersonale/MostraFile.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Aziendale
+namespace WPF_InserimentoPersonale
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MostraFile.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MostraFile : Page
     {
-        public MainWindow()
+        public MostraFile()
         {
             InitializeComponent();
         }
